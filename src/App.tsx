708f30654171f6541231cd4836b3cd08774a2b76
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-22 20:39:40 +0700

    feat: Import the component into the 'App'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Clock from './components/Counter/Clock';
+import TimerInput from './components/TimerInput/TimerInput';
 
 function App() {
   return (
     <div className="App">
+      <Clock />
+      <TimerInput />
     </div>
   );
 }

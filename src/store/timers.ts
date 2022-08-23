b416cb920cc392a299a5c827cbf6b3353f2b572b
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-23 09:21:38 +0700

    feat: Create 'timers' for mobx

---
+++ src/store/timers.ts
@@ -0,0 +1,27 @@
+import { makeAutoObservable } from 'mobx';
+
+class Timers {
+  timers: { id: number, date: string, startDate: string, completed: boolean; }[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addTimer(timer: { id: number, date: string, startDate: string, completed: boolean; }) {
+    this.timers.push(timer);
+  }
+  removeTimer(id: number) {
+    this.timers = this.timers.filter((timer: { id: number; }) => timer.id !== id);
+  }
+  completeTimer(id: number) {
+    this.timers = this.timers.map(
+      (timer: { id: number, date: string, startDate: string, completed: boolean; }) => {
+        return timer.id === id
+          ? { ...timer, completed: true }
+          : timer;
+      }
+    );
+  }
+}
+
+export default new Timers();

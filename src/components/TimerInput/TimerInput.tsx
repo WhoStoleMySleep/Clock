24b194dcbd7edd137e5017f3ebacfe73ee4c6592
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-23 09:23:51 +0700

    feat(components): Upgrade 'TimerInput'

--- src/components/TimerInput/TimerInput.tsx
+++ src/components/TimerInput/TimerInput.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import './TimerInput.module.scss';
+import React, { useState } from 'react';
+import timers from '../../store/timers';
+import styles from './TimerInput.module.scss';
 
 function TimerInput() {
-  const preventDefault = (event: { preventDefault: () => void; }) => {
-    event.preventDefault()
-  }
+  const [text, setText] = useState('');
+
+  const handleFormSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    timers.addTimer({
+      id: Math.random(),
+      date: text,
+      startDate: `${hour}:${minutes}:${seconds}`,
+      completed: false
+    });
+  };
+
+  const handleInput = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setText(event.target.value);
+  };
 
   return (
-    <form className='timer-input' onSubmit={preventDefault}>
-      <input type="text" />
+    <form className={styles['timer-input']} onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        placeholder='hh:mm:ss'
+        onChange={handleInput}
+        className={styles['timer-input__input']}
+        maxLength={9}
+      />
       <input type="submit" hidden />
     </form>
   );
